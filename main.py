991b88647cb2b445dd88d349cc1f7513849c8313
Lok-To-Cheng <qtzf74@durham.ac.uk>
2024-12-16 15:25:10 +0000

    Demo completed, moving on to adding different levels!

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import pygame
 import random
 
-from pygame import K_SPACE
-
 # INITIALISE
 pygame.init()   # initialise pygame
 clock = pygame.time.Clock()
@@ -38,7 +36,6 @@ pW, pH = 128, 128   # Sprite dimensions
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.is_moving = False
         self.sprites = []
         self.sprites.append(pygame.image.load("Images/Bill/standing.png").convert_alpha())
         self.sprites.append(pygame.image.load("Images/Bill/running_1.png").convert_alpha())
@@ -55,24 +52,28 @@ class Player(pygame.sprite.Sprite):
         self.is_facing_left = False
         self.is_moving = False
         # Player movement and velocity
-        self.x_vel = 32
-        self.step = 0
-        self.frame_per_step = 5
+        self.step = 32
+        self.x_vel = 0
+        self.frame_per_step = 3
         self.frame_count = 0
-        # Initial player hitbox
+        # Player hitbox and health
         self.hitbox = pygame.rect.Rect(self.x + 48, self.y + 16, 32, 112)
+        self.hp = 10
 
     def move(self, keys):
-        self.is_moving = True
+        self.x_vel = 0
+        self.is_moving = False
         if keys[pygame.K_d] and self.x < scrW-self.rect.w:
-            self.step = self.x_vel
+            self.x_vel = self.step
             self.is_facing_left = False
-        elif keys[pygame.K_a] and self.x > 0:
-            self.step = -self.x_vel
+            self.is_moving = True
+        if keys[pygame.K_a] and self.x > 0:
+            self.x_vel = -self.step
             self.is_facing_left = True
-        else:
-            self.step = 0
-            self.is_moving = False
+            self.is_moving = True
+        if keys[pygame.K_w]:
+            # jumping perhaps?
+            pass
 
     def shoot(self):
         return Bullet(self.x, self.y, self.is_facing_left)
@@ -81,25 +82,36 @@ class Player(pygame.sprite.Sprite):
         for e in enemies:
             if self.hitbox.x < e.hitbox.x < (self.hitbox.x + self.hitbox.w):
                 return True
+        return False
+
+    def damaged(self):
+        colourMask = pygame.Surface(self.image.get_size())
+        colourMask.fill((255, 0, 0))
+        damagedImage = self.image.copy()
+        damagedImage.blit(colourMask, (0, 0), special_flags=pygame.BLEND_MULT)
+        return damagedImage
 
     def update(self):
         self.frame_count = (self.frame_count % self.frame_per_step) + 1
         # Update player position
         if self.is_moving:
             if self.frame_count == 2:   # 2 frame delay before moving
-                self.current = (self.current % 2) + 1
-                self.x += self.step
+                self.current = (self.current % 2) + 1   # loop running animation
+                self.x += self.x_vel
         else:
             self.current = 0
         # Update player sprite image
         self.image = self.sprites[self.current]
         if self.is_facing_left:
             self.image = pygame.transform.flip(self.sprites[self.current], True, False)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (self.x, self.y)
         # Update player health points
         if self.is_hit():
-            print("Bill: Ouch!")
+            self.hp -= 1
+            self.image = self.damaged()
+        if self.hp <= 0:
+            self.kill()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
         # Update player hitbox
         self.hitbox = pygame.rect.Rect(self.x + 48, self.y + 16, 32, 112)
 
@@ -110,23 +122,24 @@ class Bullet(pygame.sprite.Sprite):
         self.sprites = []
         self.sprites.append(pygame.image.load("Images/bullets/staple.png").convert_alpha())
         self.image = self.sprites[0]
+        self.goes_left = facing_left
         # shooting point
-        if facing_left:
+        self.speed = 32
+        if self.goes_left:
             self.image = pygame.transform.flip(self.sprites[0], True, False)
-            self.x_vel = -16
+            self.x_vel = -self.speed
             self.x = x + 16
         else:
-            self.x_vel = 16
+            self.x_vel = self.speed
             self.x = x + 112
         self.y_vel = 0
-        self.y_acc = gravity   # gravitational acceleration
         self.y = y + 40
         self.rect = self.image.get_rect()
         self.rect.topleft = (self.x, self.y)
 
     def update(self):
         self.rect.x += self.x_vel
-        self.y_vel += self.y_acc
+        self.y_vel += gravity
         if self.rect.y < scrH - floorH: self.rect.y += self.y_vel
         else: self.kill()
 
@@ -134,7 +147,7 @@ class Bullet(pygame.sprite.Sprite):
 # Enemy
 eW, eH = 64, 64
 
-class Enemy(pygame.sprite.Sprite):
+class EnemyNAO(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.is_moving = False
@@ -145,17 +158,35 @@ class Enemy(pygame.sprite.Sprite):
         self.current = 0
         self.image = self.sprites[self.current]
         # Enemy position
-        self.x = random.randint(384, scrW-eW)
+        self.x = random.randint(512, scrW-eW)
         self.y = scrH - floorH - eH
         self.rect = self.image.get_rect()
         self.rect.topleft = (self.x, self.y)
         # Enemy movement
-        self.x_vel = 16
+        self.x_vel = 8
         self.step = -self.x_vel
+        self.knockback = self.x_vel
         self.frame_per_step = 5
         self.frame_count = 0
-        # Initial enemy hitbox
+        # Enemy hitbox and health
         self.hitbox = pygame.rect.Rect(self.x + 12, self.y, 40, 64)
+        self.hp = 10
+
+    def is_hit(self):
+        for bullet in bullets:
+            if (self.hitbox.x < bullet.rect.x < self.hitbox.x + self.hitbox.w) \
+                and (self.hitbox.y < bullet.rect.y < self.hitbox.y + self.hitbox.h):
+                if bullet.goes_left:
+                    self.knockback = -self.x_vel
+                return True
+        return False
+
+    def damaged(self):
+        colourMask = pygame.Surface(self.image.get_size())
+        colourMask.fill((255, 0, 0))
+        damagedImage = self.image.copy()
+        damagedImage.blit(colourMask, (0, 0), special_flags=pygame.BLEND_MULT)
+        return damagedImage
 
     def update(self):
         self.frame_count = (self.frame_count % self.frame_per_step) + 1
@@ -163,13 +194,19 @@ class Enemy(pygame.sprite.Sprite):
             # Update enemy sprite image
             self.current = (self.current + 1) % 2
             self.image = self.sprites[self.current]
-            # Update enemy position
             if self.x > 64:
                 self.x += self.step
-            self.rect = self.image.get_rect()
-            self.rect.topleft = (self.x, self.y)
-            # Update enemy hitbox
-            self.hitbox = pygame.rect.Rect(self.x + 12, self.y, 40, 64)
+        # Update enemy health
+        if self.is_hit():
+            self.hp -= 1
+            self.image = self.damaged()
+            self.x += self.knockback
+        if self.hp <= 0:
+            self.kill()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
+        # Update enemy hitbox
+        self.hitbox = pygame.rect.Rect(self.x + 12, self.y, 40, 64)
 
 movingSprites = pygame.sprite.Group()
 characterSprites = pygame.sprite.Group()
@@ -181,10 +218,10 @@ movingSprites.add(player)
 characterSprites.add(player)
 
 for i in range(5):
-    enemy = Enemy()
-    movingSprites.add(enemy)
-    characterSprites.add(enemy)
-    enemies.add(enemy)
+    enemyNAO = EnemyNAO()
+    movingSprites.add(enemyNAO)
+    characterSprites.add(enemyNAO)
+    enemies.add(enemyNAO)
 
 # GAME LOOP
 running = True
@@ -196,7 +233,7 @@ while running:
             pass
     keys_down = pygame.key.get_pressed()
     player.move(keys_down)   # user input into player movement
-    if keys_down[K_SPACE]:
+    if keys_down[pygame.K_SPACE]:
         staple = player.shoot()
         movingSprites.add(staple)
         bullets.add(staple)
